33bbfa4ccc43c35bb477131ca4b32c8a01e463da
QuantDaddy <34705068+davidkim0523@users.noreply.github.com>
2020-11-26 15:02:46 +0900

    CDS Update

--- CDS/CDS.py
+++ CDS/CDS.py
@@ -1,62 +1,15 @@
+import datetime
 import QuantLib as ql
-
-calendar = ql.UnitedStates()
+from SWAP_CURVE import GET_QUOTE, SWAP_CURVE
 
 # Today's Date
-todaysDate = ql.Date(20, 11, 2020)
-ql.Settings.instance().setEvaluationDate = todaysDate
+todaysDate = datetime.date(2020, 10, 8)
+curve = SWAP_CURVE(todaysDate, GET_QUOTE(todaysDate))
+curve_handle = ql.YieldTermStructureHandle(curve)
 
-# Swap Curve
-dep_tenors = [1, 2, 3, 6, 12]
-dep_quotes = [0.1455, 0.1761, 0.2126, 0.2555, 0.3386]
-isdaRateHelpers = [ql.DepositRateHelper(ql.QuoteHandle(ql.SimpleQuote(dep_quotes[i] / 100)), 
-                                        ql.Period(dep_tenors[i], ql.Months),
-                                        2,
-                                        calendar,
-                                        ql.ModifiedFollowing,
-                                        False,
-                                        ql.Actual360())
-                   for i in range(len(dep_tenors))]
+todaysDate = ql.Date(todaysDate.day, todaysDate.month, todaysDate.year)
 
-swap_tenors = [2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 15, 20, 25, 30]
-swap_quotes = [0.2495,
-               0.2988,
-               0.3650,
-               0.4500,
-               0.5410,
-               0.6305,
-               0.7110,
-               0.7858,
-               0.8543,
-               0.9680,
-               1.0890,
-               1.1933,
-               1.2438,
-               1.2602]
-
-isda_ibor = ql.IborIndex('IsdaIbor',
-                         ql.Period(3, ql.Months),
-                         2,
-                         ql.USDCurrency(),
-                         calendar,
-                         ql.ModifiedFollowing,
-                         False,
-                         ql.Actual360())
-
-isdaRateHelpers = isdaRateHelpers + [ql.SwapRateHelper(ql.QuoteHandle(ql.SimpleQuote(swap_quotes[i] / 100)),
-                                                       ql.Period(swap_tenors[i], ql.Years),
-                                                       calendar,
-                                                       ql.Semiannual,
-                                                       ql.ModifiedFollowing,
-                                                       ql.Thirty360(),
-                                                       isda_ibor)
-                                    for i in range(len(swap_tenors))]
-
-discountCurve = ql.RelinkableYieldTermStructureHandle()
-discountCurve.linkTo(ql.PiecewiseLinearZero(0,
-                                            calendar,
-                                            isdaRateHelpers,
-                                            ql.Actual360()))
+calendar = ql.UnitedStates()
 
 # Recovery Rate
 recovery_rate = 0.4
@@ -84,14 +37,14 @@ maturities = [calendar.adjust(todaysDate + x, ql.Following) for x in tenors]
 
 cdsHelpers = [ql.SpreadCdsHelper(ql.QuoteHandle(ql.SimpleQuote(s / 10000)),
                                  tenor,
-                                 2,
+                                 0, # >>> contract cannot start before accrual
                                  calendar,
                                  ql.Quarterly,
                                  ql.ModifiedFollowing,
                                  ql.DateGeneration.TwentiethIMM,
                                  ql.Actual360(),
                                  recovery_rate,
-                                 discountCurve)
+                                 curve_handle)
                 for s, tenor in zip(quoted_spreads, tenors)]
 
 hazard_curve = ql.PiecewiseFlatHazardRate(todaysDate,
@@ -109,8 +62,8 @@ probability = ql.DefaultProbabilityTermStructureHandle(hazard_curve)
 
 # We'll create a cds for every maturity:
 
-maturity = ql.Date(21, 11, 2025)
-spread = 21.751
+maturity = ql.Date(26, 11, 2025)
+spread = 21.755
 
 schedule = ql.Schedule(todaysDate,
                        maturity,
@@ -126,9 +79,12 @@ cds = ql.CreditDefaultSwap(ql.Protection.Buyer,
                            spread / 10000,
                            schedule,
                            ql.ModifiedFollowing,
-                           ql.Actual360())
+                           ql.Actual360(),
+                           True,
+                           True,
+                           todaysDate)
 
-engine = ql.MidPointCdsEngine(probability, recovery_rate, discountCurve)
+engine = ql.MidPointCdsEngine(probability, recovery_rate, curve_handle)
 cds.setPricingEngine(engine)
 
 print(cds.fairSpread())
