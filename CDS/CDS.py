5b4f06e1daa5952a91ac338485fdf739226d6c7f
QuantDaddy <34705068+davidkim0523@users.noreply.github.com>
2020-12-28 16:12:30 +0900

    Add files via upload

--- CDS/CDS.py
+++ CDS/CDS.py
@@ -1,58 +1,35 @@
-import datetime
 import QuantLib as ql
-from SWAP_CURVE import GET_QUOTE, SWAP_CURVE
+from CDS_CURVE import *
 
-# Today's Date
-todaysDate = datetime.date(2020, 10, 8)
-curve = SWAP_CURVE(todaysDate, GET_QUOTE(todaysDate))
-curve_handle = ql.YieldTermStructureHandle(curve)
+class CDS():
+    def __init__(self):
+        pass
+    
+    def DISCOUNT_CURVE(self):
+        pass
+    
+    def CDS_CURVE(self):
+        pass
+    
+    def PRICING(self):
+        pass
+    
+    def IR_DELTA(self):
+        pass
+    
+    def CREDIT_DELTA(self):
+        pass
+    
+    def THETA(self):
+        pass
 
-todaysDate = ql.Date(todaysDate.day, todaysDate.month, todaysDate.year)
 
 calendar = ql.UnitedStates()
 
-# Recovery Rate
-recovery_rate = 0.4
+# Today's Date
+todaysDate = ql.Date(1, 12, 2020)
+ql.Settings.instance().setEvaluationDate = todaysDate
 
-# Market Quotes
-quoted_spreads = [7.97,
-                  8.29,
-                  9.61,
-                  11.23,
-                  15.5,
-                  21.751,
-                  29.64,
-                  41.66]
-
-tenors = [ql.Period(6, ql.Months),
-          ql.Period(1, ql.Years),
-          ql.Period(2, ql.Years),
-          ql.Period(3, ql.Years),
-          ql.Period(4, ql.Years),
-          ql.Period(5, ql.Years),
-          ql.Period(7, ql.Years),
-          ql.Period(10, ql.Years)]
-
-maturities = [calendar.adjust(todaysDate + x, ql.Following) for x in tenors]
-
-cdsHelpers = [ql.SpreadCdsHelper(ql.QuoteHandle(ql.SimpleQuote(s / 10000)),
-                                 tenor,
-                                 0, # >>> contract cannot start before accrual
-                                 calendar,
-                                 ql.Quarterly,
-                                 ql.ModifiedFollowing,
-                                 ql.DateGeneration.TwentiethIMM,
-                                 ql.Actual360(),
-                                 recovery_rate,
-                                 curve_handle)
-                for s, tenor in zip(quoted_spreads, tenors)]
-
-hazard_curve = ql.PiecewiseFlatHazardRate(todaysDate,
-                                          cdsHelpers,
-                                          ql.Actual360())
-
-
-#print(hazard_curve.survivalProbability(todaysDate + ql.Period("1Y")), 0.9704)
 
 
 # ### Reprice instruments
@@ -62,8 +39,8 @@ probability = ql.DefaultProbabilityTermStructureHandle(hazard_curve)
 
 # We'll create a cds for every maturity:
 
-maturity = ql.Date(26, 11, 2025)
-spread = 21.755
+maturity = ql.Date(21, 11, 2025)
+spread = 21.4918
 
 schedule = ql.Schedule(todaysDate,
                        maturity,
@@ -81,10 +58,9 @@ cds = ql.CreditDefaultSwap(ql.Protection.Buyer,
                            ql.ModifiedFollowing,
                            ql.Actual360(),
                            True,
-                           True,
-                           todaysDate)
+                           True)
 
-engine = ql.MidPointCdsEngine(probability, recovery_rate, curve_handle)
+engine = ql.MidPointCdsEngine(probability, recovery_rate, discountCurve)
 cds.setPricingEngine(engine)
 
 print(cds.fairSpread())
@@ -92,3 +68,8 @@ print(cds.NPV())
 
 #print(cds.runningSpread())
 print(cds.fairUpfront())
+
+print(cds.conventionalSpread(0.4, discountCurve, ql.Actual360()))
+print(cds.couponLegNPV())
+print(cds.defaultLegNPV())
+
